b4b0ffeb8f982fabe7beeb9e9678c77525909f15
Stanislav Fatkhutdinov <stanislav.fatkhutdinov@gmail.com>
2025-02-15 19:17:19 -0500

    Initial Commit

---
+++ Ollama Extension/Ollama_ExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ollama_ExtensionApp.swift
+//  Ollama Extension
+//
+//  Created by Stanislav Fatkhutdinov on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Ollama_ExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
